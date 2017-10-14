0b53474390e98f795c44a6b9d042feb7c370118b
unknown <1410068@PC012.ei.ing.edu.ni>
2017-10-14 17:25:53 -0600

    modifique la parte de contacto con css

--- app/contacto/index.js
+++ app/contacto/index.js
@@ -1,47 +1,24 @@
 
 const form = `
- <div class="row">
-    <form class="col s12 center-align">
-      <div class="row">
-        <div class="input-field col s6">
-          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-          <label for="first_name">First Name</label>
-        </div>
-        <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate">
-          <label for="last_name">Last Name</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-          <label for="disabled">Disabled</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
-          <label for="password">Password</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="email" type="email" class="validate">
-          <label for="email">Email</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col s12">
-          This is an inline input field:
-          <div class="input-field inline">
-            <input id="email" type="email" class="validate">
-            <label for="email" data-error="wrong" data-success="right">Email</label>
-          </div>
-        </div>
-      </div>
-    </form>
-  </div>`
 
-const contacto = `<h1>Contáctanos</h1> ${form}`
+<img style="float:center;" src="https://estaticos.qdq.com/swdata/home_photos/579/579060506/slide4.jpg"/>
+
+<div class="correo">
+  <h6 >Correo: info94@outlook.com</h6>
+  <h6>Telefono: 2288-5566</h6>
+  <h6>Direccion: Upoli</h6>
+  <a href="https://es-la.facebook.com/login/">Facebook</a>
+</div>
+
+
+
+ `
+
+const contacto = `<h1 id="contacto">Contáctanos</h1> ${form}
+   
+
+
+    `
+
 
 export default contacto
