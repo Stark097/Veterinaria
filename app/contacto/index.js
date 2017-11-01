3d52cce460f8de33b2926342bd89b526ba5d8795
unknown <1410068@PC005.ei.ing.edu.ni>
2017-10-31 20:03:22 -0600

    inserte el mapa de busquedad en contacto, cree la base de datosla cual se va a ocupar en el proyecto e inserte las imagenes

--- app/contacto/index.js
+++ app/contacto/index.js
@@ -1,7 +1,7 @@
 
 const form = `
 
-<img style="float:center;" src="https://estaticos.qdq.com/swdata/home_photos/579/579060506/slide4.jpg"/>
+<img style="float:center;" width="900" height="250"src="https://estaticos.qdq.com/swdata/home_photos/579/579060506/slide4.jpg"/>
 
 <div class="correo">
   <h6 >Correo: info94@outlook.com</h6>
@@ -10,7 +10,8 @@ const form = `
   <a href="https://es-la.facebook.com/login/">Facebook</a>
 </div>
 
-
+  <div class="hola" ><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.622217420302!2d-86.22193228653765!3d12.137980486438877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f73fdd5fff15809%3A0x71d7df8a67b559e0!2sUniversidad+Politecnica+de+Nicaragua!5e0!3m2!1ses!2sni!4v1509120818914" 
+  width="200" height="200" frameborder="0" style="border:0" allowfullscreen></iframe></div>
 
  `
 
